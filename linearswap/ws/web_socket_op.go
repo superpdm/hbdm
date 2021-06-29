47a60ef35e5cc71782d4bf992ab4a21914d4b74b
liang guo <superpdm@gmail.com>
2021-06-29 10:23:59 +0800

    断开的时候，自动重连

--- linearswap/ws/web_socket_op.go
+++ linearswap/ws/web_socket_op.go
@@ -144,7 +144,8 @@ func (wsOp *WebSocketOp) readLoop(conn *websocket.Conn) {
 		msgType, buf, err := conn.ReadMessage()
 		if err != nil {
 			log.Error("Read error: %s", err)
-			continue
+			go wsOp.onClose(1, err.Error())
+			break
 		}
 		var message string
 		if msgType == websocket.BinaryMessage {
